9e8e5f82355d9144f73ca5dd91a25c98cafc26d3
fangyou <854016108@qq.com>
2020-07-18 21:00:27 +0800

    数据库表扩展

--- microcloud-provider-dept-8001/src/main/java/db-8001.sql
+++ microcloud-provider-dept-8001/src/main/java/db-8001.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS mldn8001;
-CREATE DATABASE mld8001 CHARACTER SET UTF8;
-USE mldn8001;
+--部门表
+--CREATE DATABASE mld8001 CHARACTER SET UTF8;
+--USE mldn8001;
 CREATE TABLE dept(
     deptno BIGINT AUTO_INCREMENT,
     dname VARCHAR(50),
@@ -12,3 +12,18 @@ INSERT INTO dept(dname,loc) VALUES ('财务部',DATABASE());
 INSERT INTO dept(dname,loc) VALUES ('市场部',DATABASE());
 INSERT INTO dept(dname,loc) VALUES ('后勤部',DATABASE());
 INSERT INTO dept(dname,loc) VALUES ('公关部',DATABASE());
+--数据字典
+CREATE TABLE sys_sjzd(
+    id BIGINT NOT NULL AUTO_INCREMENT COMMENT '主键',
+    zdbh VARCHAR(100) NOT NULL COMMENT '字典编号',
+    zddm VARCHAR(100) NOT NULL COMMENT '字典代码',
+    zddmmc VARCHAR(255) NOT NULL COMMENT '代码名称',
+    zddmz VARCHAR(255) NOT NULL COMMENT '字典名称',
+    zd_status CHAR(3) NOT NULL COMMENT '数据状态(001:启用;002:停用)',
+    CONSTRAINT pk_id PRIMARY KEY(id)
+) COMMENT = '字典表';
+INSERT INTO sys_sjzd(zdbh,zddm,zddmz,zddmmc,zd_status) VALUES ('CARDTYPE','1','SFZ','身份证','001');
+INSERT INTO sys_sjzd(zdbh,zddm,zddmz,zddmmc,zd_status) VALUES ('CARDTYPE','2','JSZ','驾驶证','001');
+INSERT INTO sys_sjzd(zdbh,zddm,zddmz,zddmmc,zd_status) VALUES ('CARDTYPE','3','SBK','社保卡','001');
+INSERT INTO sys_sjzd(zdbh,zddm,zddmz,zddmmc,zd_status) VALUES ('CARDTYPE','4','JZZ','居住证','001');
+INSERT INTO sys_sjzd(zdbh,zddm,zddmz,zddmmc,zd_status) VALUES ('CARDTYPE','5','XSZ','学生证','001');
--- microcloud-provider-login-8002/src/main/java/com/db_8002.sql
+++ microcloud-provider-login-8002/src/main/java/com/db_8002.sql
@@ -1,4 +1,5 @@
-USE mldn8001;
+--USE mldn8001;
+--用户表
 CREATE TABLE sys_user(
     id BIGINT NOT NULL AUTO_INCREMENT COMMENT '主键',
     username VARCHAR(50) NOT NULL COMMENT '用户名',
@@ -34,11 +35,19 @@ current_address,phone_number,corporate_name,dept_id,position_job,positional_titl
 document_type,id_number,marital_status,user_mode,create_time,create_by)
 VALUES ('admin','123456','854016108@qq.com',STR_TO_DATE('05.03.1990', '%m.%d.%Y'),30,'1','管理员','河南省洛阳市',
 '河南省郑州市南阳路151号院','15238679100','上海方优信息技术有限公司','16','主数据管理员','主数据管理','本科',
-'身份证','410329199004259900','2','001',NOW(),'admin');
+'SFZ','410329199004259900','2','001',NOW(),'admin');
 
 INSERT INTO sys_user(username,`password`,email,date_birth,age,sex,realname,home_address,
 current_address,phone_number,corporate_name,dept_id,position_job,positional_titles,record_schooling,
 document_type,id_number,marital_status,user_mode,create_time,create_by)
 VALUES ('fangyou','123456','11865622@qq.com',STR_TO_DATE('04.15.1990', '%m.%d.%Y'),30,'1','方磊','河南省洛阳市',
 '河南省郑州市南阳路151号院','15238679800','上海方优信息技术有限公司','16','主数据管理员','主数据管理','本科',
-'身份证','410329199005259900','2','001',NOW(),'admin');
+'SFZ','410329199005259900','2','001',NOW(),'admin');
+
+INSERT INTO sys_user(username,`password`,email,date_birth,age,sex,realname,home_address,
+current_address,phone_number,corporate_name,dept_id,position_job,positional_titles,record_schooling,
+document_type,id_number,marital_status,user_mode,create_time,create_by)
+VALUES ('lily','123456','1186555@qq.com',STR_TO_DATE('04.15.2000', '%m.%d.%Y'),20,'2','李莉','陕西省西安市莲湖区北关正街150号',
+'河南省郑州市惠济区天河路151号','15236889118','上海方优信息技术有限公司','18','招聘主管','高级企业人力资源管理师','本科',
+'XSZ','47130010092022','2','001',NOW(),'admin');
+
