2849639bbeb4840d3c9398fe6cf78de433ea6e5c
fangyou <854016108@qq.com>
2020-09-01 13:10:18 +0800

    网关组件核心功能过滤器添加

--- microcloud-gateway-5555/src/main/java/com/fangyou/Getway5555_StartSpringCloudApplication.java
+++ microcloud-gateway-5555/src/main/java/com/fangyou/Getway5555_StartSpringCloudApplication.java
@@ -1,14 +1,21 @@
 package com.fangyou;
 
+import com.fangyou.filter.AccessTokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
 public class Getway5555_StartSpringCloudApplication {
     public static void main(String[] args) {
         SpringApplication.run(Getway5555_StartSpringCloudApplication.class);
+    }
+
+    @Bean
+    public AccessTokenFilter accessFilter(){
+        return new AccessTokenFilter();
     }
 }
---
+++ microcloud-gateway-5555/src/main/java/com/fangyou/filter/AccessTokenFilter.java
@@ -0,0 +1,37 @@
+package com.fangyou.filter;
+
+import com.netflix.zuul.IZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.http.protocol.RequestContent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class AccessTokenFilter implements IZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(AccessTokenFilter.class);
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info("send {} request to {}",request.getMethod(),request.getRequestURL().toString());
+        String accessToken = request.getParameter("accessToken");
+        if(accessToken == null){
+            log.warn("access token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        log.info("access token ok");
+        return null;
+    }
+}
